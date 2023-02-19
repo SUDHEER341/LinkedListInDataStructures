feb12495ffa8da87fab88544d7c1fc50db28c97e
SUDHEER341 <sudheer601220@gmail.com>
2023-02-20 00:09:26 +0530

    Added : Ability to create  linked list by appending 30 and 70 to 56

--- Linked_List_Main.java
+++ Linked_List_Main.java
@@ -1,19 +1,18 @@
 package com.linkedlist;
-
-
 import java.util.LinkedList;
-
 public class Linked_List_Main {
         public static void main(String[] args)
 
         {
-            LinkedList<Integer> myList=new LinkedList();
-            myList.add(70);
-            System.out.println("before adding 30 and 56  : " + myList);
-            myList.addFirst(56);
-            myList.add(1,30);
+            LinkedList<Integer> myList1=new LinkedList();
+            myList1.add(56);
+            System.out.println("before adding 30 and 70 : "+myList1);
+            LinkedList<Integer> myList2=new LinkedList();
+            myList2.add(30);
+            myList2.add(70);
 
-            System.out.println("After adding 30 and 56  : " + myList);
+            myList1.addAll(myList2);
+            System.out.println("after appending 30 and 70 to 56 : "+ myList1);
 
 
 
